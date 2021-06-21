11d6badedbbb7ca05737bc78efa595cae1b6551a
Roman Didukh <newrdlink@yandex.ru>
2021-06-21 17:48:19 +0300

    start on develop branch

---
+++ src/utils/handlerRes.js
@@ -0,0 +1,11 @@
+const handleResponse = (res) => {
+  if (res.ok) {
+    return res.json();
+  }
+  if (res.status === 409) {
+    return Promise.reject(`Ошибка: ${res.status}, email занят`)
+  }
+  return Promise.reject(`Извините, ошибка: ${res.status}`);
+};
+
+export default handleResponse;
