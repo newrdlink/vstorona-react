ef3cd82f2faa2046aa66efeb70ed4e0bc8dcbdfe
Roman Didukh <newrdlink@yandex.ru>
2021-06-21 18:35:50 +0300

    start fonts adding

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="app">
+      <p className="app__title">администрация</p>
+    </div>
+  )
+}
+
+export default App
