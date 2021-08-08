d086d882f34e7d31183e6e12a80191666aa633a7
Roman Didukh <newrdlink@yandex.ru>
2021-08-08 21:12:18 +0300

    html contin...

---
+++ src/components/Main/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Main.css'
+
+import Activity from './Activity/Activity'
+import Intro from './Intro/Intro'
+
+const Main = () => {
+  return (
+    <>
+      <Intro />
+      <Activity />
+    </>
+  )
+}
+
+export default Main
