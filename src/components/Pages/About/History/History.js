683ce63ede4464130fbdf8dab9bdb464346e6cc8
Roman Didukh <newrdlink@yandex.ru>
2021-08-12 16:31:18 +0300

    history html

--- src/components/Pages/About/History/History.js
+++ src/components/Pages/About/History/History.js
@@ -3,6 +3,9 @@ import './History.css'
 import PageTitleShadow from '../../../PageTitleShadow/PageTitleShadow'
 import i1 from '../../../../images/History/1.jpg'
 import i2 from '../../../../images/History/2.jpg'
+import i3 from '../../../../images/History/3.png'
+import i4 from '../../../../images/History/4.jpg'
+import i5 from '../../../../images/History/5.jpg'
 
 
 const History = ({ pageInfo }) => {
@@ -42,6 +45,48 @@ const History = ({ pageInfo }) => {
         <p className="history__text">На первые годы существования Клуба пришелся расцвет художественной самодеятельности и любительского творчества рабочих и жителей Выборгского района, многие из которых спустя годы привели сюда своих детей, а некоторые – стали руководителями творческих коллективов.
         </p>
       </div>
+
+      <div className="history__rename">
+        <p className="history__text-span history__text-span_place_about">
+          В июне 1994 г. Клуб был  преобразован в муниципальное учреждение культуры Клуб «Выборгская сторона»
+        </p>
+        <img alt="" src={i3} className="history__rename-img" />
+      </div>
+
+
+      <div className="history__content">
+        <p className="history__text history__text_place_content">
+          На основании Распоряжения Комитета по Управлению городским имуществом Санкт-Петербурга в  2011 г. учреждение получило наименование «Санкт-Петербургское государственное бюджетное учреждение культуры «Клуб «Выборгская сторона» (СПб ГБУК «Клуб «Выборгская сторона»).
+        </p>
+        <p className="history__text history__text_place_content">
+          История Клуба тесно переплетена с историей и жизнью как жителей района и города, так и с творческой биографией известных деятелей культуры.
+          <span className="history__text-span history__text-span_place_content">Он был создан как центр досуга для работников производственного объединения по переработке пластмасс имени «Комсомольской правды».
+          </span>
+        </p>
+        <p className="history__text history__text_place_content">
+          На первые годы существования Клуба пришелся расцвет художественной самодеятельности и любительского творчества рабочих и жителей Выборгского района, многие из которых спустя годы привели сюда своих детей, а некоторые – стали руководителями творческих коллективов.
+        </p>
+      </div>
+
+
+      <div className="history__end">
+        <img alt="" src={i4} className="history__end-img" />
+        <img alt="" src={i5} className="history__end-img" />
+        <div className="history__end-content">
+          <span className="history__text-span history__text-span_place_end">
+            Клуб «Выборгская сторона» - центр культурной жизни своего микрорайона
+          </span>
+          <p className="history__text history__text_place_end">
+            С момента основания и по настоящее время Клуб не только остается центром культурной жизни своего микрорайона, но и местом проведения  крупных районных и городских мероприятий.
+          </p>
+          <p className="history__text history__text_place_end">
+            Диапазон его творческих проектов – от  занятий творческих студий до фестивалей городского и международного масштаба - сопоставим только с возрастным и социальным диапазоном посетителей: школьников, молодежи, людей старшего возраста. Идея культурного центра, открытого для всех, отвечающего самым разнообразным культурным запросам людей – по-прежнему остается главной в работе этого старейшего учреждения культуры Выборгского района.
+          </p>
+        </div>
+
+
+      </div>
+
     </section>
   )
 }
--- src/config/aboutItems.js
+++ src/config/aboutItems.js
@@ -1,12 +1,16 @@
 // import i1 from '../images/About/1.png'
-import i2 from '../images/About/2.png'
+import i1 from '../images/About/1.jpg'
+import i2 from '../images/About/2.jpg'
+import i3 from '../images/About/3.png'
+import i4 from '../images/About/4.png'
+import i5 from '../images/About/5.jpeg'
 
 export const aboutItems = [
   {
     id: 1,
     name: "История",
     path: "/history",
-    image: i2
+    image: i1
   },
   {
     id: 2,
@@ -18,18 +22,18 @@ export const aboutItems = [
     id: 3,
     name: "Документы",
     path: "/documents",
-    image: i2
+    image: i3
   },
   {
     id: 4,
     name: "Достижения",
     path: "/achievements",
-    image: i2
+    image: i4
   },
   {
     id: 5,
     name: "Анкета",
     path: "/questionnaire",
-    image: i2
+    image: i5
   }
 ]
