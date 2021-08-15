1c507baf6f01bb5b446fb6681a7b02a88ec6f4cf
Roman Didukh <newrdlink@yandex.ru>
2021-08-15 10:57:45 +0300

    start collective page

---
+++ src/components/Worker/Worker.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Worker.css'
+
+const Worker = () => {
+  return (
+    <></>
+  )
+}
+
+export default Worker
---
+++ src/components/WorkersBox/WorkersBox.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './WorkersBox.css'
+
+import Worker from '../Worker/Worker'
+
+const WorkersBox = () => {
+  return (
+    <ul className="workers-box">
+
+    </ul>
+  )
+}
+
+export default WorkersBox
