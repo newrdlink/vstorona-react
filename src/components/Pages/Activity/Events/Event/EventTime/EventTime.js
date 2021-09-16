3e4e4a138c2545841b32f74556b3a0f9ad66ae35
Roman Didukh <newrdlink@yandex.ru>
2021-09-16 12:25:22 +0300

    add event hard

--- src/components/Pages/Activity/Events/Event/EventTime/EventTime.js
+++ src/components/Pages/Activity/Events/Event/EventTime/EventTime.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import './EventTime.css'
 
-const EventTime = ({ isActive, time }) => {
+import getMonth from '../../../../../../helpers/getMonthEvent'
+
+const EventTime = ({ isActive, startTime, endTime }) => {
+  // console.log(typeof startTime)
+  const startTimeEvent = new Date(startTime)
+  const endTimeEvent = new Date(endTime)
+  // console.log(startTimeEvent.getDate())
+
+  const eventDay = startTimeEvent.getDate()
+
+  const eventMonth = startTimeEvent.getMonth() + 1
+  console.log(getMonth(eventMonth))
 
   return (
     <div className="event-time">
-      <p>{time}</p>
+      <p className="event-time__day">{eventDay}</p>
+      <p className="event-time__month-year">{`${getMonth(eventMonth)} ${startTimeEvent.getFullYear()} г.`}</p>
+      <p className="event-time__current-time">
+        {`${startTimeEvent.getHours()}.${startTimeEvent.getMinutes()} - ${endTimeEvent.getHours()}.${endTimeEvent.getMinutes()}`}
+      </p>
     </div>
   )
 }
