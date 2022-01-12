f5afecef50a392aff2c7f02fd3b0a242b222468f
Roman Didukh <newrdlink@yandex.ru>
2022-01-13 01:54:03 +0300

    add icons

--- src/config/hallsItems.js
+++ src/config/hallsItems.js
@@ -1,36 +1,39 @@
 // import i1 from '../images/About/1.png'
-import i1 from '../images/Services/1.jpg'
-import i2 from '../images/Services/2.jpg'
+import bigHall from '../images/Services/HallsIcons/bigHallIcon.png'
+import choreographyHall from '../images/Services/HallsIcons/choreographyHallIcon.png'
+import foyerHallIcon from '../images/Services/HallsIcons/foyerHallIcon.png'
+import showroomHallIcon from '../images/Services/HallsIcons/showroomHallIcon.png'
+import sixHallIcon from '../images/Services/HallsIcons/sixHallIcon.png'
 
 export const hallsItems = [
   {
     id: 1,
     name: "большой зал",
     path: "/big",
-    image: i1
+    image: bigHall
   },
   {
     id: 2,
     name: "фойе 1 этажа",
     path: "/foyer",
-    image: i2
+    image: foyerHallIcon
   },
   {
     id: 3,
     name: "выставочный зал",
     path: "/showroom",
-    image: i1
+    image: showroomHallIcon
   },
   {
     id: 4,
     name: "зал хореографии",
     path: "/choreography",
-    image: i2
+    image: choreographyHall
   },
   {
     id: 5,
     name: "зал 6",
     path: "/costume",
-    image: i2
+    image: sixHallIcon
   },
 ]
