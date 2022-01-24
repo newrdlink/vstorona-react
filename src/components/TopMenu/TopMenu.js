f96c790a4544078dc3e3384ccf2bb447c37edcbf
Roman Didukh <newrdlink@yandex.ru>
2022-01-24 21:18:36 +0300

    add main image

--- src/components/TopMenu/TopMenu.js
+++ src/components/TopMenu/TopMenu.js
@@ -38,7 +38,7 @@ const TopMenu = ({ isActive, isTopMenuNoActive, onClickBtnClose, onClickLink }) 
 
       </div>
 
-      <Link to="/anti-corruption" className="top-menu__link" onClick={onClickLink}>противоднйствие коррупции</Link>
+      <Link to="/anti-corruption" className="top-menu__link" onClick={onClickLink}>противодействие коррупции</Link>
       <Link to="/accessible" className="top-menu__link" onClick={onClickLink}>доступная среда</Link>
       <Link to="/contacts" className="top-menu__link" onClick={onClickLink}>контакты</Link>
 
