0f27df2811011f4bf5433476fb075a66ab0528c1
Roman Didukh <newrdlink@yandex.ru>
2022-01-26 22:19:05 +0300

    fix anticorrcontainer

--- src/components/Pages/Corruption/DocContainer/DocContainer.js
+++ src/components/Pages/Corruption/DocContainer/DocContainer.js
@@ -9,14 +9,17 @@ const DocContainer = ({ items = [], loggedIn, removeDoc }) => {
   const [isActive, setIsActive] = useState(false)
 
   return (
-    <section className={`doc-items-container ${isActive && "doc-items-container_active"}`}>
+    <section
+      className={`doc-items-container doc-items-container_place_anticorr
+      ${isActive && "doc-items-container_active"}`}>
       <DocBoxTitle
         title={items.length === 0 ? "" : items[0].type}
         place="anti-corr-docs"
         onClick={() => setIsActive(!isActive)}
       // onClick={() => console.log(1)}
       />
-      <ul className={`doc-items ${isActive && "doc-items_active"}`}>
+      <ul className={`doc-items doc-items_place_anticorr
+      ${isActive && "doc-items_active"}`}>
         {
           items.map(item => <DocItemCorr key={item._id} item={item} loggedIn={loggedIn} removeDoc={removeDoc} />)
         }
